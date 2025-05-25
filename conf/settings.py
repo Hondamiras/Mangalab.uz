0cc2917ece735d0269d2981230f58d3e7ad7ceee
Hondamir <157780018+Hondamiras@users.noreply.github.com>
2025-05-25 14:20:18 +0500

    database

--- conf/settings.py
+++ conf/settings.py
@@ -156,5 +156,5 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = ["*"]  # Лучше указать домен render
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(default=f'sqlite:///{BASE_DIR}/db.sqlite3', conn_max_age=600)
 }
