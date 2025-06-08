fe385fb893e599deff720e75b14ec6dfee5e802a
Hondamir <157780018+Hondamiras@users.noreply.github.com>
2025-06-08 21:40:46 +0500

    s

--- manga/urls.py
+++ manga/urls.py
@@ -7,7 +7,7 @@ app_name = "manga"
 urlpatterns = [
     path("", views.manga_list, name="manga_list"),
     path("<slug:manga_slug>/", views.manga_details, name="manga_details"),
-    path("<slug:manga_slug>/jild/<int:volume>/bob/<int:chapter_number>/",views.chapter_read, name="chapter_read"),
+    path("<slug:manga_slug>/jild/<int:volume>/bob/<int:chapter_number>/", views.chapter_read, name="chapter_read"),
     path('chapter/<int:chapter_id>/thank/',views.thank_chapter, name='thank_chapter'),
     path('<slug:manga_slug>/add/', views.add_to_reading_list, name='add_to_reading_list'),
 ]
