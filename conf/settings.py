b32c0e120115df28f7598ef0a8e6fcd7bc4c487b
Hondamir <157780018+Hondamiras@users.noreply.github.com>
2025-08-15 23:22:39 +0500

    Update settings.py

--- conf/settings.py
+++ conf/settings.py
@@ -14,6 +14,8 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
+WHITENOISE_MANIFEST_STRICT = False
+
 ALLOWED_HOSTS = [
     # '*',
     
